5995e97635bd4f547d7d43c505b36df26e591908
Thembeka Nguza <Thembeka.Nguza@ekurhuleni.gov.za>
2023-07-20 15:26:20 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MiddleBarComponent } from './middle-bar/middle-bar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolBarComponent,
+    MiddleBarComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/middle-bar/middle-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-middle-bar',
+  templateUrl: './middle-bar.component.html',
+  styleUrls: ['./middle-bar.component.scss']
+})
+export class MiddleBarComponent {
+
+}
